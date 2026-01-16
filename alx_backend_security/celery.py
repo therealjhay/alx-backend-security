d797051a22332cce333a0449e60af981b68b18e7
Johnson Oyemade <johnsonoca@gmail.com>
2026-01-16 23:30:58 +0100

    error fixed

--- alx_backend_security/celery.py
+++ alx_backend_security/celery.py
@@ -1,7 +1,7 @@
 import os
 from celery import Celery
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend_security.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "alx_backend_security.settings")
 
 app = Celery("alx_backend_security")
 app.config_from_object("django.conf:settings", namespace="CELERY")
